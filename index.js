4b43d4923eb28d1d3c31f4134b95340a87490adf
KarenOn <55865352+KarenOn@users.noreply.github.com>
2021-05-13 23:57:59 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+require("./routes/citaRoute.js")(app);
+
+app.listen(process.env.PORT || 3080, () => {
+    console.log('Servidor escuchando en la 3080');
+});
